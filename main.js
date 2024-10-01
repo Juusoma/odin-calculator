73062da14871e4b5eff50e7f4fef5441c28ed7c7
Juuso Määttä <juusomaatta@hotmail.fi>
2024-10-01 19:42:18 +0300

    Add operate function

--- main.js
+++ main.js
@@ -14,7 +14,26 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-console.log(`add 3 and 5: ${add(3, 5)}`);
-console.log(`subtract 3 and 5: ${subtract(3, 5)}`);
-console.log(`multiply 3 and 5: ${multiply(3, 5)}`);
-console.log(`divide 3 and 5: ${divide(3, 5)}`);
+function operate(operator, num1, num2){
+    switch(operator){
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            debug.error(`Invalid operator: ${operator}`);
+    }
+}
+
+console.log(`add 3 and 5: ${operate("+", 3, 5)}`);
+console.log(`subtract 3 and 5: ${operate("-", 3, 5)}`);
+console.log(`multiply 3 and 5: ${operate("*", 3, 5)}`);
+console.log(`divide 3 and 5: ${operate("/", 3, 5)}`);
+
+let memSlot1 = 0;
+let memSlot2 = 0;
+let operator = "";
