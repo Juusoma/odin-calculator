803654fdb279e2305d6b75126632d6ece4c2c66e
Juuso Määttä <juusomaatta@hotmail.fi>
2024-10-01 19:33:00 +0300

    Add add, subtract, multiple, and divide functions
    Verify them with console logs

---
+++ main.js
@@ -0,0 +1,20 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+console.log(`add 3 and 5: ${add(3, 5)}`);
+console.log(`subtract 3 and 5: ${subtract(3, 5)}`);
+console.log(`multiply 3 and 5: ${multiply(3, 5)}`);
+console.log(`divide 3 and 5: ${divide(3, 5)}`);
