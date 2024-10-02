ded91db9ef13e2d88917834ee4d9843cd8a400ec
Juuso Määttä <juusomaatta@hotmail.fi>
2024-10-02 17:22:26 +0300

    Make buttons work
    Input characters to the display with the calculator buttons

--- main.js
+++ main.js
@@ -37,3 +37,69 @@ console.log(`divide 3 and 5: ${operate("/", 3, 5)}`);
 let memSlot1 = 0;
 let memSlot2 = 0;
 let operator = "";
+let displayValue = "";
+const maxDisplayCharacters = 7;
+
+const calculatorDisplayElement = document.querySelector(".calculator-screen");
+
+function setDisplayText(str){
+    calculatorDisplayElement.textContent = str;
+}
+
+function clearDisplay(){
+    displayValue = "";
+    setDisplayText("");
+}
+
+function addCharacterToDisplay(num){
+    if(displayValue.length >= maxDisplayCharacters){
+        return;
+    }
+
+    displayValue += num;
+    setDisplayText(displayValue);
+}
+
+const calculatorKeypadElement = document.querySelector(".calculator-keypad");
+calculatorKeypadElement.addEventListener("click", handleKeypadClick);
+
+function handleKeypadClick(e){
+    switch(e.target.id){
+        case "button-clear":
+            clearDisplay();
+            break;
+        case "button-nine":
+            addCharacterToDisplay(9);
+            break;
+        case "button-eight":
+            addCharacterToDisplay(8);
+            break;
+        case "button-seven":
+            addCharacterToDisplay(7);
+            break;
+        case "button-six":
+            addCharacterToDisplay(6);
+            break;
+        case "button-five":
+            addCharacterToDisplay(5);
+            break;
+        case "button-four":
+            addCharacterToDisplay(4);
+            break;
+        case "button-three":
+            addCharacterToDisplay(3);
+            break;
+        case "button-two":
+            addCharacterToDisplay(2);
+            break;
+        case "button-one":
+            addCharacterToDisplay(1);
+            break;
+        case "button-zero":
+            addCharacterToDisplay(0);
+            break;
+        case "button-decimal":
+            addCharacterToDisplay(".");
+            break;
+    }
+}
